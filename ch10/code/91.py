1b29e38d105cd34a1023c675c32d3e16175090fd
t-sako1126 <skd.151143@gmail.com>
2026-02-17 15:10:51 +0900

    91

---
+++ ch10/code/91.py
@@ -0,0 +1,19 @@
+from transformers import AutoTokenizer,AutoModelForCausalLM
+import torch
+
+device="cuda"
+MODEL="gpt2"
+PROMPT="The movie was full of"
+
+tok=AutoTokenizer.from_pretrained(MODEL)
+model=AutoModelForCausalLM.from_pretrained(MODEL).to(device).eval()
+
+imput=tok(PROMPT,return_tensors="pt").to(device)
+
+def generate_text(**kwargs):
+    generate_id=model.generate(**imput,max_new_tokens=40,pad_token_id=tok.eos_token_id,**kwargs)
+    print(tok.decode(generate_id[0],skip_special_tokens=True),"\n")
+
+generate_text(do_sample=False)
+generate_text(do_sample=True,temperature=0.5)
+generate_text(do_sample=True,temperature=1.5)
