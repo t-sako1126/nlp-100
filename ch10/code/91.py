7fe969876f3cac62705d7c952d160ae12c289592
t-sako1126 <skd.151143@gmail.com>
2026-02-17 15:11:55 +0900

    91

--- ch10/code/91.py
+++ ch10/code/91.py
@@ -8,10 +8,10 @@ PROMPT="The movie was full of"
 tok=AutoTokenizer.from_pretrained(MODEL)
 model=AutoModelForCausalLM.from_pretrained(MODEL).to(device).eval()
 
-imput=tok(PROMPT,return_tensors="pt").to(device)
+input=tok(PROMPT,return_tensors="pt").to(device)
 
 def generate_text(**kwargs):
-    generate_id=model.generate(**imput,max_new_tokens=40,pad_token_id=tok.eos_token_id,**kwargs)
+    generate_id=model.generate(**input,max_new_tokens=40,pad_token_id=tok.eos_token_id,**kwargs)
     print(tok.decode(generate_id[0],skip_special_tokens=True),"\n")
 
 generate_text(do_sample=False)
